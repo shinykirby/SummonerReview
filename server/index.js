33d23dd86155bbc46038b22f65a061ca6f0637ad
mhollenbeck <moritzhollenbeck@gmx.de>
2022-07-31 22:11:53 +0200

    set up basic routes, controller, schema

--- server/index.js
+++ server/index.js
@@ -3,5 +3,19 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 
-const express = require("express");
+import postRoutes from './routes/posts.js'
 
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://reviewdatabaseuser1:fr3OiplOrVpWmPfN@cluster0.ujic7.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log('Server running on port: ' + PORT)))
+    .catch((error) => console.log(error.message));
