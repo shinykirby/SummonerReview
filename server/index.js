6ef069b4492e96759507e46f0e897fb5a7f4ca95
jonny <jonikeule@web.de>
2022-08-10 15:02:31 +0200

    posts now use postMessage form

--- server/index.js
+++ server/index.js
@@ -16,6 +16,10 @@ app.use(cors());
 const CONNECTION_URL = 'mongodb+srv://reviewdatabaseuser1:fr3OiplOrVpWmPfN@cluster0.ujic7.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
-    .then(() => app.listen(PORT, () => console.log('Server running on port: ' + PORT)))
-    .catch((error) => console.log(error.message));
+app.listen(PORT, () => console.log('Server running on port: ' + PORT));
+
+//mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+//    .then(() => app.listen(PORT, () => console.log('Server running on port: ' + PORT)))
+//    .catch((error) => console.log(error.message));
+
+//mongoose.set('useFindAndModify', false);
